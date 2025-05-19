5507ba0de8838cb8051c922ab08720ec9ea13157
TbHund <timofeibogutski@gmail.com>
2025-05-19 21:25:52 +0300

    детали товара

--- OnlineShop/main/admin.py
+++ OnlineShop/main/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from .models import Size, Category, \
-    ClothingItem, ClothingItemSize
+    ClothingItem, ClothingItemSize, ItemImage
 
 
 class ClothingItemSizeInLine(admin.TabularInline):
     model = ClothingItemSize
     extra = 4
     
+class ItemImageInLine(admin.TabularInline):
+    model = ItemImage
+    extra = 4
+
 @admin.register(Size)
 class SizeAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -28,4 +32,4 @@ class ClothingItemAdmin(admin.ModelAdmin):
     list_filter = ('available', 'category')
     prepopulated_fields = {'slug': ('name',)}
     ordering = ('-created_at', )
-    inlines = [ClothingItemSizeInLine]
+    inlines = [ClothingItemSizeInLine, ItemImageInLine]
