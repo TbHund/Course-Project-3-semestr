3d0d8222a56f92adb48851a7314fd4234fe89034
TbHund <timofeibogutski@gmail.com>
2025-05-19 22:13:14 +0300

    каталог и детали пофикшены

--- OnlineShop/main/views.py
+++ OnlineShop/main/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import ClothingItem, Category, Size, \
     ClothingItemSize
@@ -17,23 +16,30 @@ class CatalogView(ListView):
         size_names = self.request.GET.getlist('size')
         min_price = self.request.GET.get('min_price')
         max_price = self.request.GET.get('max_price')
+        search_query = self.request.GET.get('q')
 
-        if category_slugs:
+        if category_slugs: 
             queryset = queryset.filter(category__slug__in=category_slugs)
 
         if size_names:
             queryset = queryset.filter(
                 Q(sizes__name__in=size_names) & Q(sizes__clothingitemsize__available=True)
-            )
-
+            ).distinct()
+        
         if min_price:
             queryset = queryset.filter(price__gte=min_price)
 
         if max_price:
-            queryset = queryset.filter(price__lte=max_price)    
+            queryset = queryset.filter(price__lte=max_price)
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(description__icontains=search_query)
+            ).distinct()
 
         return queryset
-
+    
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -45,8 +51,7 @@ class CatalogView(ListView):
         context['max_price'] = self.request.GET.get('max_price', '')
 
         return context
-
-
+    
 
 class ClothingItemDetailView(DetailView):
     model = ClothingItem
@@ -55,8 +60,7 @@ class ClothingItemDetailView(DetailView):
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
 
-
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         clothing_item = self.object
