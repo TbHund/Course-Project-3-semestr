e311eeb098f4c1caf43d668e8d4dc57787be0402
TbHund <timofeibogutski@gmail.com>
2025-05-23 17:24:18 +0300

    шаблоны регистрации

--- OnlineShop/OnlineShop/urls.py
+++ OnlineShop/OnlineShop/urls.py
@@ -8,6 +8,7 @@ app_name = 'users'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('users/', include('users.urls', namespace='users')),
     path('', include('main.urls'))
 ]
 
